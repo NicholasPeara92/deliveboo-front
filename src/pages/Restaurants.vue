b7d33fe14fe57c8f7572a5f9cf172fc4411cc4be
nembo93 <francesco.c193@libero.it>
2023-02-22 15:18:24 +0100

    Stampa card con i ristoranti in pagina Restaurants

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -1,7 +1,14 @@
 <template>
-<h1>Qui ci mettiamo i ristoranti</h1>
-<div>
-    <div v-for="restaurant in restaurants">{{ restaurant.name }}</div>
+<h1>Ristoranti:</h1>
+<div class="restaurants_container">
+    <div v-for="restaurant in restaurants" class="restaurants_card">
+        <h4>{{ restaurant.name }}</h4>
+        <ul>
+            <li>Tel: {{ restaurant.telephone }}</li>           
+            <li>Categoria: {{ restaurant.category }}</li>
+        </ul>
+        <a href="#">Guarda il menu</a>
+    </div>
 </div>
 </template>
 
@@ -27,5 +34,17 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.restaurants_container{
+    padding: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.restaurants_card{
+    min-width: 250px;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid black;
+}
 </style>
