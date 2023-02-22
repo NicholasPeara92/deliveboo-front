818685bf9c38d951b4c5cf1acc980367aa0c156e
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-02-22 17:27:47 +0100

    aggiunta sez partner

---
+++ src/components/main/Partner.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="partner">
+        <div class="section">
+            <h1>Diventa nostro partner!</h1>
+            <h3>Raggiungi più clienti con DeliveBoo! Gestiamo noi la consegna, così tu puoi dedicarti a offrire i migliori piatti e prodotti</h3>
+                <router-link :to="{ name: 'register' }">
+                    <button> Inizia ora!</button>
+                </router-link>
+            </div>
+        <img src="../../assets/partner.PNG" alt="" />
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container-fluid px-0">
     <Hero />
+    <Partner />
   </div>
 </template>
 
 <script>
 import Hero from "../components/main/Hero.vue";
+import Partner from "../components/main/Partner.vue";
 export default {
   name: "HomePage",
   components: {
     Hero,
+    Partner,
   },
 };
 </script>
