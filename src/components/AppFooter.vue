367ee776780342caa5b3c662d3186d49a6e71bca
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-02-22 15:55:32 +0100

    bozza footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,17 +1,61 @@
-<template>
-  <div>
-    <h2 class="footer_container">Footer</h2>
-  </div>
-</template>
 
-<script>
-export default {
-  name: "AppFooter",
-};
+<script setup>
+import { ref } from 'vue'
 </script>
 
+<!--------------------------------->
+<template>
+  <div class="body">
+    <div class="container">
+      <div class="lista">
+        <ul>
+          <li><h5>Scopri DeliveBoo</h5></li>
+          <li><a href="*">Chi siamo</a></li>
+          <li><a href="*">Ristoranti</a></li>
+          <li><a href="*">Diventa Nostro partner</a></li>
+        </ul>
+      </div>
+      <div class="lista">
+        <ul>
+          <li><h5>Aiuto</h5></li>
+          <li><a href="*">Contatti</a></li>
+          <li><a href="*">FAQ</a></li>
+        </ul>
+      </div>
+      <div class="lista">
+        <ul>        
+          <li><h5>Porta DeliveBoo con te</h5></li>
+          <li><a href="*">PlayStore</a></li>
+          <li><a href="*">AppStore</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<!--------------------------------->
 <style lang="scss" scoped>
-.footer_container{
-  border: 2px solid black;
+.body{
+  background-color: #2e3333;
+  overflow: hidden;
+  height: 300px;
 }
+.container{
+  display: flex;
+  justify-content: space-between;
+    .lista{
+      align-items:center; 
+      color: white;
+      padding-top: 50px ;
+        ul{
+          background-color: #434848;
+          list-style-type: none;
+          height: 150px;
+          width: 250px;
+          margin-top: 20px;
+          }
+        a{
+          color: #00cdbc;
+        }
+      }
+    }
 </style>
