bf7142cf554d0633d7ab08c433c3c50758f76c30
NicholasPeara92 <nicholas.peara@gmail.com>
2023-02-22 16:14:15 +0100

    Aggiunto padding sezione restaurant

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -1,15 +1,17 @@
 <template>
-<h1>Ristoranti:</h1>
-<div class="restaurants_container">
-    <div v-for="restaurant in restaurants" class="restaurants_card">
+  <section>
+    <h1>Ristoranti:</h1>
+    <div class="restaurants_container">
+      <div v-for="restaurant in restaurants" class="restaurants_card">
         <h4>{{ restaurant.name }}</h4>
         <ul>
-            <li>Tel: {{ restaurant.telephone }}</li>           
-            <li>Categoria: {{ restaurant.category }}</li>
+          <li>Tel: {{ restaurant.telephone }}</li>
+          <li>Categoria: {{ restaurant.category }}</li>
         </ul>
         <a href="#">Guarda il menu</a>
+      </div>
     </div>
-</div>
+  </section>
 </template>
 
 <script>
@@ -18,33 +20,35 @@ import { store } from "../store";
 
 export default {
   name: "Restaurants",
-  data(){
-    return{
-        store,
-        restaurants:[],
+  data() {
+    return {
+      store,
+      restaurants: [],
     };
   },
-  
+
   created() {
     axios.get(`${this.store.api_url}/restaurants`).then((response) => {
       this.restaurants = response.data;
     });
   },
-
 };
 </script>
 
 <style scoped>
-.restaurants_container{
-    padding: 30px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+section {
+  padding-top: 100px;
 }
-.restaurants_card{
-    min-width: 250px;
-    padding: 10px;
-    margin-bottom: 20px;
-    border: 1px solid black;
+.restaurants_container {
+  padding: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.restaurants_card {
+  min-width: 250px;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid black;
 }
 </style>
