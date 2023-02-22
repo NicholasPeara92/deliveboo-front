9d17d15b9d9a7e35b31089f5177b00500f8c2783
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-02-22 17:37:54 +0100

    fix btn

--- src/components/main/Partner.vue
+++ src/components/main/Partner.vue
@@ -6,7 +6,7 @@
         Raggiungi più clienti con DeliveBoo! Gestiamo noi la consegna, così tu
         puoi dedicarti a offrire i migliori piatti e prodotti
       </h3>
-      <button><a href="http://127.0.0.1:8000/register"></a> Inizia ora!</button>
+      <button><a href="http://127.0.0.1:8000/register">inizia ora!</a></button>
     </div>
     <img src="../../assets/partner.PNG" alt="" />
   </section>
@@ -16,4 +16,5 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
