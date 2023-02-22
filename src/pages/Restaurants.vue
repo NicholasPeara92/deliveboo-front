02cd68ff5fdb2dbe626d051c9321a439c441b4c0
NicholasPeara92 <nicholas.peara@gmail.com>
2023-02-22 16:19:19 +0100

    fix restaurant

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -1,24 +1,22 @@
 <template>
-<h1>Ristoranti:</h1>
-<div class="restaurants-container">
+  <h1>Ristoranti:</h1>
+  <div class="restaurants-container">
     <div v-for="restaurant in restaurants" class="restaurants-card">
-        <h4>{{ restaurant.name }}</h4>
-        <div>
-            <img :src="restaurant.image" alt="">
-        </div>
-        <ul>
-            <li>Tel: {{ restaurant.telephone }}</li> 
-            <li>
-                <ul>
-                    <li v-for="category in restaurant.categories">{{ category }}</li>
-                </ul>
-            </li>
-            
-        </ul>
-        <a href="#">Guarda il menu</a>
+      <h4>{{ restaurant.name }}</h4>
+      <div>
+        <img :src="restaurant.image" alt="" />
       </div>
+      <ul>
+        <li>Tel: {{ restaurant.telephone }}</li>
+        <li>
+          <ul>
+            <li v-for="category in restaurant.categories">{{ category }}</li>
+          </ul>
+        </li>
+      </ul>
+      <a href="#">Guarda il menu</a>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -43,17 +41,17 @@ export default {
 </script>
 
 <style scoped>
-.restaurants-container{
-    padding: 30px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+.restaurants-container {
+  padding: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
-.restaurants-card{
-    min-width: 250px;
-    padding: 10px;
-    margin-bottom: 20px;
-    border: 1px solid black;
+.restaurants-card {
+  min-width: 250px;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid black;
 }
 .restaurants_card {
   min-width: 250px;
