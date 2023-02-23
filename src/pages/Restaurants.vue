f8d6a67e883ce3626d46350d2d0d7253e857d05a
nembo93 <francesco.c193@libero.it>
2023-02-23 18:09:34 +0100

    Aggiunge card bootsrapt a pagina ristoranti

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -28,11 +28,44 @@
           </li>
         </ul>
         <!-- <router-link :to="{ name: 'menu' }">Guarda il menu</router-link> -->
-
         <router-link :to="{ name: 'menu', params: { slug: restaurant.slug } }"
           >Guarda il menu</router-link
         >
       </div>
+
+      <!-- CARD -->
+      <div class="card mb-3 px-1 py-1" style="max-width: 540px;"
+      v-for="(restaurant, index) in restaurantList"
+      :key="index"
+      >
+        <div class="row g-0">
+          <div class="col-md-4 d-flex justify-content-center">
+            <img :src="restaurant.image_url" class="img-fluid rounded-start my-class" alt="...">
+          </div>
+          <div class="col-md-8 position-relative">
+            <div class="card-body">
+              <h5 class="card-title">{{ restaurant.name }}</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <!-- Bottone guarda il menu -->
+              <router-link :to="{ name: 'menu', params: { slug: restaurant.slug } }"
+              class="btn btn-primary"
+              >Guarda il menu</router-link
+              >
+              <!-- Fine bottone -->
+            </div>
+            <!-- Badge categorie -->
+            <div class="d-flex position-absolute top-0 end-0">
+              <span
+              v-for="category in restaurant.categories"
+              class="badge bg-warning text-dark me-1">{{ category.name }}
+              </span>
+            </div>
+            <!-- Fine badge categorie -->
+          </div>
+        </div>
+      </div>
+      <!-- FINE CARD -->
+
     </div>
   </div>
 </template>
@@ -104,5 +137,9 @@ export default {
   padding: 10px;
   margin-bottom: 20px;
   border: 1px solid black;
+}
+
+.my-class{
+  object-fit: contain;
 }
 </style>
