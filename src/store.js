a1593479b1cd15e1ca7a9aa012a60e950f7dfa0b
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-02-27 19:16:08 +0100

    carrello che si svuota andando su un altro menu

--- src/store.js
+++ src/store.js
@@ -21,6 +21,18 @@ export const store = reactive({
       });
       localStorage.products = JSON.stringify(this.products);
     } else {
+      axios.get(`${this.api_url}/restaurant/` + "" + param).then((response) => {
+        this.restaurant = response.data;
+        this.products = this.restaurant.products.sort((c1, c2) =>
+          c1.type > c2.type ? 1 : c1.type < c2.type ? -1 : 0
+        );
+        this.products.forEach((element) => {
+          element.quantity = 0;
+          element.totalPrice = 0;
+        });
+        console.log(this.products);
+      });
+      localStorage.products = JSON.stringify(this.products);
     }
   },
   addToCart(id) {
