d89ef62d1b7e1df6e184417434b09c630835f1fa
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-02-28 17:56:52 +0100

    fixato responsive homepage

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -177,7 +177,7 @@ export default {
   transition: all 0.3s;
   @media only screen and (max-width: 576px) {
     width: 100%;
-    height: 240px;
+    height: 200px;
     padding-top: 20px;
     .ms-badge {
       flex-wrap: nowrap;
@@ -194,7 +194,7 @@ h1 {
     margin-top: 150px;
   }
   @media only screen and (max-width: 576px) {
-    margin-top: 200px;
+    margin-top: 30px;
   }
 }
 
