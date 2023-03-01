0f3ab58624c1961b4cfd0e3320a0f94d308a22bb
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-03-01 13:19:54 +0100

    sistemazione pagina order

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -1,89 +1,97 @@
 <script>
-
+import { store } from "../store";
 export default {
   name: "Order",
   data() {
     return {
+      store,
     };
   },
-}
+};
 </script>
 
-<template>    
-<form>
-  
-  <div class="segment">
-    <h1>Inserisci i tuoi dati</h1>
-  </div>
+<template>
+  <form>
+    <div class="segment">
+      <h1>Inserisci i tuoi dati</h1>
+    </div>
 
-    <input type="text" placeholder="Email Address"/>
-    <input type="text" placeholder="Nome"/>
-    <input type="text" placeholder="Cognome">
-    <input type="text" placeholder="Indirizzo di consegna">
-    <input type="text" placeholder="Telefono">
-    <h3>totale</h3>
-  
+    <input class="my-2" type="text" placeholder="Email Address" />
+    <input class="my-2" type="text" placeholder="Nome" />
+    <input class="my-2" type="text" placeholder="Cognome" />
+    <input class="my-2" type="text" placeholder="Indirizzo di consegna" />
+    <input class="my-2" type="text" placeholder="Telefono" />
+    <h3 class="my-2">Totale: {{ store.getTotalPrice() }} €</h3>
 
-  <button class="red" type="button"><a href=""></a>Acquista</button>
-  <router-link :to="{ name: 'homepage' }"><button class="red" type="button">Torna alla HomePage</button></router-link>
-  
-</form>
-
+    <button class="red my-3" type="button"><a href=""></a>Acquista</button>
+    <router-link :to="{ name: 'homepage' }"
+      ><button class="red my-3" type="button">
+        Torna alla HomePage
+      </button></router-link
+    >
+  </form>
 </template>
 
 <style scoped lang="scss">
 $ruler: 16px;
-$color-red: #AE1100;
-$color-bg: #EBECF0;
-$color-shadow: #BABECC;
-$color-white: #FFF;
+$color-red: #ae1100;
+$color-bg: #ebecf0;
+$color-shadow: #babecc;
+$color-white: #fff;
 
-body, html {
-  background-color:$color-bg;
+body,
+html {
+  background-color: $color-bg;
 }
 
-body, p, input, select, textarea, button {
-    font-family: 'Montserrat', sans-serif;
-    letter-spacing: -0.2px;
-    font-size: $ruler;
+body,
+p,
+input,
+select,
+textarea,
+button {
+  font-family: "Montserrat", sans-serif;
+  letter-spacing: -0.2px;
+  font-size: $ruler;
 }
 
-div, p {
+div,
+p {
   color: $color-shadow;
   text-shadow: 1px 1px 1px $color-white;
 }
 
-
 form {
   padding: $ruler;
-  width: $ruler*20;
+  width: $ruler * 20;
   margin: 0 auto;
 }
 
 .segment {
-  padding: $ruler*2 0;
+  padding: $ruler * 2 0;
   text-align: center;
 }
 
-button, input {
+button,
+input {
   border: 0;
   outline: 0;
   font-size: $ruler;
-  border-radius: $ruler*20;
+  border-radius: $ruler * 20;
   padding: $ruler;
-  background-color:$color-bg;
+  background-color: $color-bg;
   text-shadow: 1px 1px 0 $color-white;
 }
 
 label {
   display: block;
-  margin-bottom: $ruler*1.5;
+  margin-bottom: $ruler * 1.5;
   width: 100%;
 }
 
 input {
   margin-right: 8px;
-  box-shadow:  inset 2px 2px 5px $color-shadow, inset -5px -5px 10px $color-white;
+  box-shadow: inset 2px 2px 5px $color-shadow, inset -5px -5px 10px $color-white;
   width: 100%;
   box-sizing: border-box;
   transition: all 0.2s ease-in-out;
@@ -91,50 +99,52 @@ input {
   -webkit-appearance: none;
 
   &:focus {
-    box-shadow:  inset 1px 1px 2px $color-shadow, inset -1px -1px 2px $color-white;
+    box-shadow: inset 1px 1px 2px $color-shadow,
+      inset -1px -1px 2px $color-white;
   }
 }
 
 button {
-  color:#61677C;
+  color: #61677c;
   font-weight: bold;
-  box-shadow: -5px -5px 20px $color-white,  5px 5px 20px $color-shadow;
+  box-shadow: -5px -5px 20px $color-white, 5px 5px 20px $color-shadow;
   transition: all 0.2s ease-in-out;
   cursor: pointer;
   font-weight: 600;
-  
+
   &:hover {
     box-shadow: -2px -2px 5px $color-white, 2px 2px 5px $color-shadow;
   }
-  
+
   &:active {
-    box-shadow: inset 1px 1px 2px $color-shadow, inset -1px -1px 2px $color-white;
+    box-shadow: inset 1px 1px 2px $color-shadow,
+      inset -1px -1px 2px $color-white;
   }
-  
+
   .icon {
     margin-right: 8px;
   }
-  
+
   &.unit {
     border-radius: $ruler/2;
     line-height: 0;
-    width: $ruler*3;
-    height: $ruler*3;
-    display:inline-flex;
+    width: $ruler * 3;
+    height: $ruler * 3;
+    display: inline-flex;
     justify-content: center;
-    align-items:center;
+    align-items: center;
     margin: 0 8px;
-    font-size: $ruler*1.2;
-    
+    font-size: $ruler * 1.2;
+
     .icon {
-      margin-right: 0; 
+      margin-right: 0;
     }
   }
-  
+
   &.red {
     display: block;
     width: 100%;
-    color:$color-red;
+    color: $color-red;
   }
 }
 
@@ -142,7 +152,7 @@ button {
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  
+
   label {
     margin: 0;
     flex: 1;
