66de4695afdbc7da810e95c86b7604b73b0e02e4
NicholasPeara92 <nicholas.peara@gmail.com>
2023-03-04 18:43:32 +0100

    Aggiunge immagini del team

--- src/components/main/AboutUs.vue
+++ src/components/main/AboutUs.vue
@@ -5,8 +5,11 @@
     </div>
     <div class="container-cards">
       <div class="card">
-        <img src="../../assets/person 2.png" alt="" />
-        <h5>Pino</h5>
+        <img
+          src="https://media.licdn.com/dms/image/C5603AQECVKK6WWkyWQ/profile-displayphoto-shrink_800_800/0/1637349939483?e=1683158400&v=beta&t=K5F22VWzFNeCgy0n08RKOutcwCtrxV_E3xoiMCXm5ek"
+          alt=""
+        />
+        <h5>Nicolò</h5>
         <p>
           <i class="fa-solid fa-quote-left"></i> Oggi Deliveboo è presente in 11
           mercati in tutto il mondo, collabora con migliaia di ristoranti e
@@ -15,8 +18,11 @@
         </p>
       </div>
       <div class="card">
-        <img src="../../assets/person 1.png" alt="" />
-        <h5>Giulia</h5>
+        <img
+          src="https://media.licdn.com/dms/image/D4E03AQGhoZYG3EoBZA/profile-displayphoto-shrink_800_800/0/1675231921326?e=1683158400&v=beta&t=saDubWqsVO_9wSnjrxc4WLsIv0jOyhOwU3FFl7VNJ4Y"
+          alt=""
+        />
+        <h5>Nicholas</h5>
         <p>
           <i class="fa-solid fa-quote-left"></i> Da anni lavoriamo sodo per
           capire cosa vogliono i clienti e per offrire una proposta adatta a
@@ -25,8 +31,11 @@
         </p>
       </div>
       <div class="card">
-        <img src="../../assets/person 3.png" alt="" />
-        <h5>Antonino</h5>
+        <img
+          src="https://media.licdn.com/dms/image/D4E03AQHspQdojDpKtQ/profile-displayphoto-shrink_800_800/0/1676309100546?e=1683158400&v=beta&t=c95bzmKczHwQrWZRPorbFOdUFRjkYqpHAU7gfIfcWJo"
+          alt=""
+        />
+        <h5>Angelo</h5>
         <p>
           <i class="fa-solid fa-quote-left"></i> Il nostro modello ha fatto
           aumentare esponenzialmente il numero di ristoranti che offrono le
@@ -34,8 +43,11 @@
         </p>
       </div>
       <div class="card">
-        <img src="../../assets/person 4.png" alt="" />
-        <h5>Andrea</h5>
+        <img
+          src="https://media.licdn.com/dms/image/D5603AQHbjq8DM1CX9Q/profile-displayphoto-shrink_800_800/0/1675283544185?e=1683158400&v=beta&t=BVGRz2mi1PGWUGMQdz3T0ysb5clmf6L7XD6CXh6kfr8"
+          alt=""
+        />
+        <h5>Alessandro</h5>
         <p>
           <i class="fa-solid fa-quote-left"></i> La tecnologia sviluppata da
           Deliveboo permette di portare a termine contemporaneamente migliaia di
@@ -44,9 +56,9 @@
       </div>
       <div class="card">
         <img src="../../assets/person 5.png" alt="" />
-        <h5>Camilla</h5>
+        <h5>Francesco</h5>
         <p>
-          <i class="fa-solid fa-quote-left"></i> La passione di Deliveroo è il
+          <i class="fa-solid fa-quote-left"></i> La passione di Deliveboo è il
           cibo, con l'obiettivo di fornire alle persone una scelta sempre più
           ampia di ristoranti e di attività legate al cibo.
           <i class="fa-solid fa-quote-right"></i>
